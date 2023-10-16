c9497a8a026de9cb739ea3608b13113d72086bdb
EDITH_Coder <edithmashapo@gmail.com>
2023-10-16 13:33:56 +0200

    Add project files.

---
+++ 6B_Range/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6B_Range
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(areInRange(11, 20, 12));
+            Console.WriteLine(areInRange(30, 30, 17));
+            Console.WriteLine(areInRange(25, 35, 50));
+            Console.WriteLine(areInRange(15, 12, 8));
+            Console.ReadLine();
+        }
+        //Method to check whether three given integer values are in the range 20..50 inclusive.
+        //Return true if 1 or more of them are in the said range otherwise false.
+        public static bool areInRange( int num1,int num2,int num3)
+        {
+            if(num1>=20 && num1 <= 50 || num2>=20 && num2 <=50 || num3>=20 && num3<=50)
+            {
+                return true;
+            }
+            return false;
+
+        }
+    }
+}
